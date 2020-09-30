662dd469ea44772f757acaa0e1ac3e32626d3d27
Blake Lam <blakelam@bllg.attlocal.net>
2020-09-30 15:34:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { listings } from "./data";
+import JobListing from "./components/JobListing";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    listings,
+  };
+  render() {
+    return (
+      <>
+        <header className="header"></header>
+        <div className="wrapper">
+          {Object.keys(this.state.listings).map((key) => (
+            <JobListing key={key} details={this.state.listings[key]} />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
