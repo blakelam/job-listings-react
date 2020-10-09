5774c4ad1b91280ab0a54b237b313cf92e185564
Blake Lam <blake@blakelam.com>
2020-10-09 13:42:59 -0500

    Fix filter functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import JobsList from "./components/JobsList";
 import TagsList from "./components/TagsList";
 import Header from "./components/Header";
@@ -8,60 +8,65 @@ import { listings } from "./data";
 import { getTags } from "./helpers";
 
 function App() {
-  const [jobs, setJobs] = useState(listings);
+  const [state, setState] = useState({jobs: listings, tags: []});
+  const allJobs = listings;
 
-  /*
-    ! This is to correct an error that was blocking the Netlify build. I wasn't using the setSelectedTags function, and the build was throwing an error. Need to figure out if the selected tags need to be stored in state and/or if they need to be updated differently. 
-  */
-  const selectedTagsVar = useState([]);
-  let selectedTags = selectedTagsVar[0];
-
-  /*
-    * filterList() filters job list to only show jobs with set tags (called when a tag is clicked).
-    ! Bug - If two tags are selected (example: Frontend and Senior), removing one of the tags does not update the list. This is due to how useState() works, just not sure how to fix yet.
-  */
-  function filterList(event) {
-    // Reset
-
+  // Updates selected tags list when a tag is clicked
+  function updateTags(event) {
     // Get the value of the tag that was clicked
     const tag = event.target.dataset.tagValue;
-    setJobs(listings);
 
+    // Create a new tags list starting with all of the currently selected tags
+    let selectedTags = [...state.tags];
+
+    // Update the tag list
     if (selectedTags.includes(tag)) {
       // If clicked tag is already in the array, remove it
-      const i = selectedTags.indexOf(tag);
-      selectedTags.splice(i, 1);
+      selectedTags = selectedTags.filter((tagInList) => tagInList !== tag);
     } else {
       // If clicked tag isn't already in the array, add it
       selectedTags.push(tag);
     }
+    
+    // Filter the jobs list based on the selected tags
+    const filteredJobs = filterJobs(selectedTags);
 
-    // If any tags are selected, set state to only include filtered job listings
-    if (selectedTags.length > 0) {
-      let filteredJobs = [];
-      // Try filter
-      jobs.forEach((job) => {
-        //console.table(filteredJobs);
+    setState({jobs: filteredJobs, tags: selectedTags});
+  }
+
+  function filterJobs(tagsList) {
+    // Start with an empty list of matching jobs
+    let matchingJobs = [];
+
+    // If no tags are selected, set matching jobs to all jobs
+    // Else, add jobs with matching tags to the matching jobs list
+    if (tagsList.length === 0) {
+      matchingJobs = [...allJobs];
+    } else {
+      allJobs.forEach((job) => {
         let match = true;
         const jobTags = getTags(job);
-        selectedTags.forEach((selectedTag) => {
+        tagsList.forEach((selectedTag) => {
           if (!jobTags.includes(selectedTag)) {
             match = false;
           }
         });
-        if (match) filteredJobs.push(job);
+        if (match) matchingJobs.push(job);
       });
-      setJobs(filteredJobs);
-      console.log(filteredJobs.length);
     }
+    return matchingJobs;
   }
 
   return (
     <>
       <Header />
       <Wrapper>
-        <TagsList selectedTags={selectedTags} filterList={filterList} />
-        <JobsList jobs={jobs} filterList={filterList} getTags={getTags} />
+        <TagsList selectedTags={state.tags} updateTags={updateTags} />
+        <JobsList
+          jobs={state.jobs}
+          updateTags={updateTags}
+          getTags={getTags}
+        />
         <Footer />
       </Wrapper>
     </>
